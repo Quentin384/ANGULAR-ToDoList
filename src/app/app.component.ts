6d2cccc3c9f8c17b74b75fc1e97b0f3c09ed4060
Quentin384 <zampieri.quentin38@gmail.com>
2025-04-29 13:31:32 +0200

    Starting the TODOLIST

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChildComponent } from './child/child.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ChildComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ToDoList';
+  title = 'MyToDoList';
 }
+
